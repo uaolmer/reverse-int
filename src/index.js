d153568fc4a1b3ac194519a9c1dca7b3be41883e
uaolmer <uaolmer2@gmail.com>
2020-10-25 05:46:33 +0200

    feat: make it 1 line

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 module.exports = function reverse (n) {
-  n = n < 0 ? n * -1 : n;
-  return n.toString().split('').reverse().join('');
+  return Math.abs(n).toString().split('').reverse().join('');
 }
